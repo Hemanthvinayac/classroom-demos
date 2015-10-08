edd4ce31f16dfaee51446edf0d743a813bf22e63
hemanth <hemanth@hemanth-laptop>
2015-10-08 18:12:23 +0530

    vinay

--- 01 Java Basics/src/com/brillio/training/entity/Person.java
+++ 01 Java Basics/src/com/brillio/training/entity/Person.java
@@ -2,6 +2,7 @@ package com.brillio.training.entity;
 
 public class Person {
 
+	public int more;
 	public int val;
 	 public String name; // 8 bytes for reference
 	 public int age; // 4 bytes for int
